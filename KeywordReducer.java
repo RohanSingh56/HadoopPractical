bb2c3df027ca89520518bbba3db33ecec99281a6
Rohan <68508563+RohanSingh56@users.noreply.github.com>
2021-06-17 10:51:55 +0530

    Update and rename FrequencyReducer.java to KeywordReducer.java

--- KeywordReducer.java
+++ KeywordReducer.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class FrequencyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class KeywordReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 	public void reduce(Text key, Iterable<IntWritable> values, Context context)
 			throws IOException, InterruptedException {
